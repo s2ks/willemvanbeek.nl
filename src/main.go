76239c10219f3176ee9997c12990d510a08d78eb
jilles <bakkerjilles@gmail.com>
2019-07-22 14:31:46 +0200

    v0.1

--- src/main.go
+++ src/main.go
@@ -42,92 +42,13 @@ func img_grid_handler(w http.ResponseWriter, r *http.Request) {
 			"/static/img/20160909_125120.jpg",
 			"/static/img/20160909_125120.jpg",
 			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20160909_125120.jpg",
 			},
 			{
 			"Foto",
 			"Foto",
 			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
-			"Foto",
 			},
 			{
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
-			"text",
 			"text",
 			"text",
 			"text",
@@ -152,10 +73,10 @@ func intro_handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	t, err := template.ParseFiles(
-		prefix + "opt1/intro.html",
-		prefix + "opt1/header.html",
-		prefix + "opt1/navbar.html",
-		prefix + "opt1/footer.html",
+		prefix + "intro.html",
+		prefix + "header.html",
+		prefix + "navbar.html",
+		prefix + "footer.html",
 	)
 
 	if err != nil {
@@ -164,68 +85,10 @@ func intro_handler(w http.ResponseWriter, r *http.Request) {
 
 	data := struct {
 		Title string
-		Images []string
-		ImgLabel []string
-		ImgDesc []string
+		PageName string
 	} {
 		Title: "Willem van Beek",
-		Images: []string {
-			"/static/img/20160909_125120.jpg",
-			"/static/img/20161223_150024.jpg",
-			"/static/img/20170427_143305.jpg",
-		},
-		ImgLabel: []string {
-			"Foto 1",
-			"Foto 2",
-			"Foto 3",
-		},
-		ImgDesc: []string {
-			"Beschrijving Foto 1",
-			"Beschrijving Foto 2",
-			"Beschrijving Foto 3",
-		},
-	}
-
-	err = t.Execute(w, data)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func opt1_handler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/opt1" {
-		log.Print("404 " + r.URL.Path)
-		http.NotFound(w, r)
-		return
-	}
-
-	t, err := template.ParseFiles(
-		prefix + "opt1/index.html",
-		prefix + "opt1/header.html",
-		prefix + "opt1/navbar.html",
-		prefix + "opt1/footer.html",
-	)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	data := struct {
-		Title string
-		ImageSrc []string
-		ImageTitle []string
-		ImageDescription []string
-	} {
-		Title: "Willem van Beek",
-		ImageSrc: []string {
-		},
-		ImageTitle: []string {
-
-		},
-		ImageDescription: []string {
-
-		},
+		PageName: "Introductie",
 	}
 
 	err = t.Execute(w, data)
@@ -246,6 +109,7 @@ func root_handler(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles(
 		prefix + "index.html",
 		prefix + "header.html",
+		prefix + "navbar.html",
 		prefix + "footer.html",
 	)
 
@@ -255,8 +119,10 @@ func root_handler(w http.ResponseWriter, r *http.Request) {
 	//TODO load this data from a database
 	data := struct {
 		Title string
+		PageName string
 	} {
 		Title: "Willem van Beek",
+		PageName: "Home",
 	}
 
 	err = t.Execute(w, data)
@@ -274,7 +140,6 @@ func main() {
 	}
 
 	http.HandleFunc("/", root_handler)
-	http.HandleFunc("/opt1", opt1_handler)
 	http.HandleFunc("/introductie", intro_handler)
 	http.HandleFunc("/beelden/steen", img_grid_handler)
 
