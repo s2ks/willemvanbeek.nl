a0790794d66de87feb7f73b15220ae1e665e587f
jilles <bakkerjilles@gmail.com>
2020-02-13 20:22:56 +0100

    corrected FCGI_CONFIG path

--- exports.sh
+++ exports.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export FCGI_IDENT=willemvanbeek.nl
-export FCGI_CONFIG=~/.config/willemvanbeek.nl/
+export FCGI_CONFIG=~/.config/willemvanbeek.nl/fcgi-server.json
 export FCGI_DATABASE=~/.config/willemvanbeek.nl/fcgi-server.db
 export FCGI_WEBROOT=/srv/http/
+
